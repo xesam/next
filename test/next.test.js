503a4345478095e0d6a80fec908e427fa93e23b8
xesam <xesamguo@gmail.com>
2021-02-02 17:31:33 +0800

    0.0.2 new

---
+++ test/next.test.js
@@ -0,0 +1,19 @@
+const next = require('../src/index');
+
+const app = next();
+app.use(function (next, option) {
+
+}).use(function (next, option) {
+
+}).use(function (next, option) {
+
+}).use(function (next, option) {
+
+}).use(function (next, option) {
+
+});
+
+app({
+    name: 'xesam',
+    age: 18
+});
